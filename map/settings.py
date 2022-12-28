a8325d8dd9479db4020764642885a9a2ce51fbf1
fatemefouladkar <fatemeflk@chmail.ir>
2022-12-28 09:27:40 +0330

    database config added

--- map/settings.py
+++ map/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'map.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'map',
+        'USER': 'map_user',
+        'PASSWORD': '1234',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
