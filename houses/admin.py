67a33e0037e8824e2462acccecf964fcc219267c
fatemefouladkar <fatemeflk@chmail.ir>
2022-12-28 11:00:15 +0330

    house models and modeladmin added

--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
 
-# Register your models here.
+from.models import House
+
+
+@admin.register(House)
+class HouseAdmin(GISModelAdmin):
+    list_display = ('id', 'number', 'bedrooms','location',)
+    
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_zoom': 8,
+            'default_lat': 50.324422739309384,
+            'default_lon': 8.887939453125,
+        },
+    }
--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,11 @@
+from django.contrib.gis.db import models as gis_models
 from django.db import models
 
-# Create your models here.
+
+class House(models.Model):
+    number = models.CharField(max_length=100, null=True, blank=True)
+    bedrooms = models.IntegerField(null=True, blank=True)
+    location = gis_models.PointField(srid=4326,null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"house {self.number} with {self.bedrooms} bedrooms"
